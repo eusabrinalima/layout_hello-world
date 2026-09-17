6777caf104f47d060ab0d5141b5e8264a3f32c23
Sabrina Lima <sbrnlima18@gmail.com>
2026-09-16 21:39:22 -0300

    Updates

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20260916-212851\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20260916-213830\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
